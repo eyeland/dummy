d8361744a817cae147a552e64a9555c2eac954c0
eyeland <63192606+eyeland@users.noreply.github.com>
2022-05-31 03:16:50 -0400

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,8 @@
+//Globals - NO WINDOW !!!
+
+//__dirname    - path to current directory
+//__filename   - file name
+// require     - function to use modules(CommonJS)
+//module       - info about the current module(file)
+//process      - info about env where the program is being executed
+
